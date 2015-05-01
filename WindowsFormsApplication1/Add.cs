8ee7bb44a1f79658acd7f4ae63848635400ec403
Joachim Nisce <kimnisce@rocketmail.com>
2015-05-01 15:29:11 +0800

    My first commit

---
+++ WindowsFormsApplication1/Add.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Add : Form
+    {
+        public Add()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty) //walang laman
+            {
+                MessageBox.Show("Please input name.");
+                return;
+
+            }
+            if (textBox2.Text == string.Empty)
+	        {
+                MessageBox.Show("Please input contact number");
+                return; //terminate function immediately
+	        }
+            
+            DialogResult = DialogResult.OK;
+            //public function to export values
+            
+        }
+        public void export(out string name, out string contactNo)
+        {
+            name = textBox1.Text;
+            contactNo = textBox2.Text;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1 //namespace parang package. Package in java
+{
+    public partial class Form1 : Form
+    {
+        public string name;
+        public string contactNo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var xyz = new Add();
+            if (xyz.ShowDialog() == DialogResult.OK)
+	        {
+                xyz.export(out name, out contactNo);
+                listView1.Items.Add(this.name);
+                listView1.Items[listView1.Items.Count -1].SubItems.Add(contactNo);
+
+	        }
+        
+            
+            
+        }
+        
+    }
+}
